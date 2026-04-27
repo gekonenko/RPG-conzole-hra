7295f4dc90e0011b04393fd1ce7d154781446aa2
gekonenko <gekonenko3@gmail.com>
2026-04-27 10:24:10 +0200

    verze 1.1
    pár fixů

---
+++ RPG hra-1.1.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    int mHP;
+    int HP = 1;
+    int mmana = 0;
+    int mana;
+	int dodge = 0;
+    int poloha = 0;
+    int win = 0;
+    int turn = 0;
+    int trida;
+    string name;
+    int stopka;
+    cout << "Toto je turn-based textove RPG erpegia\n";
+
+    cout << "zadejte jmeno hrace: ";
+    cin >> name;
+
+	cout << "zadejte tridu: 1-fighter, 2-mage, 3-thief: ";
+    do {
+        cin >> trida;
+        if (trida <= 0 or trida > 3) {
+            cout << "neplatna trida, zadejte znovu: ";
+		}
+    } while (trida <= 0 or trida > 3);
+
+
+
+    cout << "______________________\n";
+    if (trida == 1) {
+        HP = 40;
+        mana = 10;
+        dodge = 3;
+        cout << "vase trida je: fighter\n";
+        cout << "vase base staty:\nHP:" << HP << "\nmana: " << mana << "\ndodge: " << dodge << "\n";
+
+    }
+    if (trida == 2) {
+        HP = 25;
+        mana = 40;
+        dodge = 5;
+		cout << "vase trida je: Mage\n";
+        cout << "vase base staty:\nHP:" << HP << "\nmana: " << mana << "\ndodge: " << dodge << "\n";
+    }
+    if (trida == 3) {
+        HP = 20;
+        mana = 25;
+        dodge = 15;
+        cout << "vase trida je: zlodej\n";
+		cout << "vase base staty:\nHP:"  << HP << "\nmana: " << mana << "\ndodge: " << dodge << "\n";
+    }
+
+
+
+
+
+
+
+
+
+
+    if (name == "loser") {
+        HP = 0;
+        win = 0;
+
+    }else if (name == "winner") {
+        HP = 1;
+        win = 1;
+	}else if (name== "cheater") {
+        HP = 1;
+        win = 1;
+	}
+
+
+
+    do {
+        turn++;
+        cout <<"______________________" << "\n";
+        cin >> stopka;
+        if (stopka == 0)
+            HP = 0;
+
+
+        cout << HP;
+        cout << win;
+
+    } while (HP > 0 and win==0);
+
+    cout<<"\n\n\n\n______________________\n";
+    if (HP <= 0) {
+        cout << " #####   #####  ###    ### #######\n##       ##   ## ####  #### ##     \n##   ### ####### ## #### ## #####  \n##    ## ##   ## ##  ##  ## ##     \n ######  ##   ## ##      ## #######\n\n ######  ##    ## ####### ######   \n##    ## ##    ## ##      ##   ##  \n##    ## ##    ## #####   ######   \n##    ##  ##  ##  ##      ##   ##  \n ######    ####   ####### ##   ##";
+    }
+    else if (win == 1) {
+        cout << "######   #####  ###    ### #######\n##       ##   ## ####  #### ##     \n##   ### ####### ## #### ## #####  \n##    ## ##   ## ##  ##  ## ##     \n ######  ##   ## ##      ## #######\n\n##      ##  ######  ###    ##\n##      ## ##    ## ####   ##\n##   #  ## ##    ## ## ##  ##\n##  ### ## ##    ## ##  ## ##\n ###   ###  ######  ##   ####\n";
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
